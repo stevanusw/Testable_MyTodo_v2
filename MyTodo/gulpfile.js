3d3604a5e102052d0e208c4f921883fd543f8e4a
stevanuz <wibowo@hotmail.com>
2019-01-02 13:37:32 +1100

    Add project files.

---
+++ MyTodo/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/// <binding AfterBuild='default' />
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var paths = {
+    src: [
+       'scripts/src/*.js'
+    ]
+};
+
+gulp.task('debug', function () {
+});
+
+gulp.task('release', function () {
+    return gulp.src(paths.src)
+        .pipe(concat('mytodo.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('scripts/dist'));
+});
+
+gulp.task('build', gulp.parallel('debug', 'release'));
+
+gulp.task('default', gulp.parallel('build'));
